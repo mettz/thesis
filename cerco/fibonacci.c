7f75bcb0016eda0ea878f7574ecf4caaa86e02c6
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2021-09-29 00:55:00 +0200

    Minor fixes

--- cerco/fibonacci.c
+++ cerco/fibonacci.c
@@ -5,21 +5,6 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-int fib_it(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    int a = 1;
-    int b = 1;
-    int i;
-    for (i = 2; i < n; i++) {
-        int c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
-}
-
-int main() {
-  return fib_it(10);
-}
+// int main() {
+//   return fib(10);
+// }
